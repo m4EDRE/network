ee72cdc0e6730cab8cca9cbdaa8b210c3da45118
Mateus Biscardi <103404542+m4EDRE@users.noreply.github.com>
2025-12-03 19:59:53 -0300

    Update and rename netscan.py to kyotoping.sh

--- kyotoping.sh
+++ kyotoping.sh
@@ -1,17 +1,9 @@
-import socket
-import os
+#!/bin/bash
 
-def pingsweep(subnet):
-    for i in range(240,256):
-        if os.system(f"ping -c 1 {subnet}{i} | grep 64") == False:
-            print(f"IP {subnet}{i} está ativo!")
+#Shebang do bash
 
-def portscan(IP):
-    for i in range(1,65353):
-        os.system(f"nc -zv 192.168.1.250 {i} 2>&1 | grep succeeded")
-        
-menu = int(input(''' 
-⠀⠀⠀⠀⣠⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣄⠀⠀⠀⠀
+echo "
+⠀⠀⠀⣠⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣄⠀⠀⠀⠀
 ⠀⠀⠀⠸⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣇⠀⠀⠀
 ⢀⣤⣤⣼⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣿⣿⣿⣷⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣤⣤⡀
 ⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⢀⣴⡞⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣦⡀⠀⠀⠀⠀⢀⣴⣶⣿⣿⣿⣿⣿⣿⣿
@@ -33,21 +25,47 @@ menu = int(input('''
 ⠀⠀⠀⠀⠀⠙⠛⢻⣿⣿⣿⣿⡆⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⢠⣿⣿⣿⣿⡟⠛⠋⠁⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠈⠿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠁⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠚⠋⠀⠀⠀⠀⠀⠀⠀⠀
-      
-Feeling....
 
-PingSweep - (1)
-PortScan  - (2)
-Selecione a opção: '''))
+Feeling...."
 
-if menu == 1:
-    print("Formato da Subnet: 172.16.0. (Por padrão é analisado o /24)")
-    subnet = input('''Repasse a Subnet a ser analisada:
-''' )
-    pingsweep(subnet)
-elif menu == 2 :
-    ip = input("Passe o host a ser analisado:")
-    portscan(ip)
-else:
-    print("Selecione uma opção válida")
+#1. Solicita e Armazena o Prefixo da Rede
+echo " "
+echo " "
+
+echo "Passe a rede a ser analisada (ex:192.168.1):
+
+"
+read ip
+
+
+
+echo "Você passou --> $ip
+
+"
+echo "--- Iniciando Pingsweep Paralelo na rede $ip.0/24 ---"
+
+#2. Cria loop
+
+for i in {1..254}; do
+
+        #3. Cria variável
+        definitive="$ip.$i"
+
+        #4. Faz ping com a variável criada
+        ping -c 1 -w 1 "$definitive" > /dev/null 2>&1
+        
+        #Comando tem tempo limite de 1 segundo para retornar algo com -w 1
+        #Comando manda a saída padrão para > /dev/null e com 2>&1 ele manda a saída de erro pro mesmo lugar
+
+        if [ $? -eq 0 ]; then
+            echo -e "\033[32m[ATIVO]\033[0m Host $definitive respondeu."
+        fi
+        
+        # $? --> Variável que armazena o código de saída do ultimo comando executado
+        # 0 é o código de saída de SUCESSO
+        # Para comparar dois valores é necessário -eq, == seria comparação de string
+        
+
+done
+
 
